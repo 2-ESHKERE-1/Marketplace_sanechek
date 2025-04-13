ffba84a94b3c0c30cebe6fb7e105711d3a193545
[] <s4393631@gmail.com>
2025-04-13 14:24:07 +0300

    йцукнгшщз

--- script.js
+++ script.js
@@ -1,97 +1,91 @@
-let productsGrid = document.getElementById("products-grid");
+let productsGrid = document.getElementById('products-grid');
 let productsArray = [];
 
 let xhr = new XMLHttpRequest();
 
-let url = "https://my-json-server.typicode.com/2-ESHKERE-1/WebMidSun14_Sanechek"
+let url = "https://my-json-server.typicode.com/2-ESHKERE-1/WebMidSun14_Sanechek";
 
-xhr.open("GET", url + "/producs");
-xhr.responseType = "json";
+xhr.open('GET', url + '/products');
+xhr.responseType = 'json';
 
-xhr.onload = function(){
+xhr.onload = function() {
     productsArray = xhr.response;
     productsGrid.innerHTML = null;
-
     productsArray.forEach(p => {
-    let pElem = document.createElement("div");
-    pElem.classList.add("product");
+        let pElem = document.createElement('div');
+        pElem.classList.add('product');
 
-    pElem.innerHTML = `
-        <h2 class="product-name">${p.name}</h2>
-        <img class="product-photo" src="${p.photo}" alt="${p.name}">
-        <p class="product-price"><b>${p.price} BYN</b></p>
-        <p class="product-desc"><b>${p.desk}</b></p>
-        <button onclick="addProductToCard(${p.id})">Buy</button>
-    `
+        pElem.innerHTML = `
+            <h2 class='product-name'>${p.name}</h2>
+            <img class='product-photo' src='${p.photo}' alt='${p.name}'>
+            <p class='product-price'><b>Price: </b>${p.price}BYN</p>
+            <p class='product-description'><b>Description: </b>${p.desc}</p>
+            <a href="userProfile.html?id=${p.author_id}">Seller profile</a>
+            <button onclick="addProductToCart(${p.id})">Buy</button>
+        `;
 
-
-    productsGrid.append(pElem) //append добавить дочерние элементы
-
+        productsGrid.append(pElem);
     });
 }
 
 xhr.send();
 
-function addProductToCard(id) {
-    xhr.open("GET", `${url}/producs/${id}`);
-    xhr.responseType = "json"; //responseType = "json"; задаёт типа ответа который придёт на запрос
-    xhr.onload = function(){
-        
-    }
-}
+//--------------------------------------------------
 
-let cartProd = document.getElementById("cart-products");
+let cartProd = document.getElementById('cart-products');
+
 let cart = [];
 
-if (localStorage.getItem("cart")){
-    cart=JSON.parse(localStorage.getItem("cart"));
+if (localStorage.getItem('cart')) {
+    cart = JSON.parse(localStorage.getItem('cart'));
 
-    drawCardProducts()
+    drawCartProducts();
 }
 
-function addProductToCard(id){
-    let product = productsArray.find(function(p){
+function addProductToCart(id) {
+    let product = productsArray.find(function(p) {
         return p.id == id;
     })
+
     cart.push(product);
 
+    drawCartProducts();
 
- drawCardProducts();
+    localStorage.setItem('cart', JSON.stringify(cart));
 
- localStorage.setItem("cart", JSON.stringify(cart));
+    document.getElementById('cart-button').classList.add('active');
+    setTimeout(function() {
+        document.getElementById('cart-button').classList.remove('active');
+    }, 500);
+}
 
- document.getElementById("cart-button").classList.add('active');
- setTimeout(function(){
-    document.getElementById("cart-button").classList.remove('active');
- },500);
-};
-
-function drawCardProducts(){
-
-    if (cart.length === 0) return cartProd.innerHTML = "Cart is empty";//если 0 продуктов пишем карт бла бла
+function drawCartProducts() {
+    if (cart.length === 0) return cartProd.innerHTML = 'Cart is empty';
 
     cartProd.innerHTML = null;
     let sum = 0;
 
-    cart.forEach(function(p){
-        cartProd.innerHTML +=`
-        <p><img src="${p.photo}"> ${p.name} | ${p.price}</p>
+    cart.forEach(function(p) {
+        cartProd.innerHTML += `
+            <p><img src="${p.photo}"> ${p.name} | ${p.price}$</p>
+            <hr>
         `;
         sum += p.price;
-    })// эта функция добовляет цену и картинку товара который мы купили
-cartProd.innerHTML +=`
-    <p><b>Total price: ${sum}</b></p>
-    <button onclick="BuyAll()">Buy all</button>
-`;
+    })
+
+    cartProd.innerHTML += `
+        <p>Total Price: ${sum}$</p>
+        <button onclick="buyAll()">Buy All</button>
+    `;
 }
 
-function BuyAll(){
+function buyAll() {
+    console.log("buyall");
     cart = [];
-    cartProd.innerHTML = "ВЫ ПОПАЛИСЬ Хаха ТЕПЕРЬ ВЫ ДОЛЖНЫ МНЕ 1000000$ МУГАГАГАГА";
+    cartProd.innerHTML = 'Money was withdraw from your credit card';
     localStorage.setItem('cart', '[]');
 }
 
-function openCart(){
-    cartProd.classList.toggle("hide");
-
+function openCart() {
+    cartProd.classList.toggle('hide');
 }
